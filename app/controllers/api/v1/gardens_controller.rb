9490c83e978f2204ce21e43b6465656071c2538b
TSephanya Brun <tsbrun@icloud.com>
2021-04-20 10:54:14 -0400

    garden controller index and create routes with actions

--- app/controllers/api/v1/gardens_controller.rb
+++ app/controllers/api/v1/gardens_controller.rb
@@ -1,2 +1,22 @@
 class Api::V1::GardensController < ApplicationController
+
+    def index 
+        @gardens = Garden.all 
+        render json: @gardens
+    end
+
+    def create 
+        garden = Garden.new(garden_params)
+        if garden.save 
+            render json: garden, status: accepted
+        else 
+            render json: {errors: garden.errors.full_messages}, status: unprocessible_entity
+        end
+    end
+
+    private 
+
+    def garden_params 
+        params.require(:garden)#.permit(:user_id)???
+    end
 end
