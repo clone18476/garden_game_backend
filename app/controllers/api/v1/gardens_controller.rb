cce69c4888ce871ce8d30598dc16a6e1be514f4c
TSephanya Brun <tsbrun@icloud.com>
2021-04-27 15:16:48 -0400

    find or create garden

--- app/controllers/api/v1/gardens_controller.rb
+++ app/controllers/api/v1/gardens_controller.rb
@@ -7,8 +7,8 @@ class Api::V1::GardensController < ApplicationController
     end
 
     def create 
-        garden = Garden.new(garden_params)
-        if garden.save 
+        garden = Garden.find_or_create_by(garden_params)
+        if garden 
             render json: garden#, status: accepted => causes NameError (undefined local variable or method `accepted' for #<Api::V1::GardensController:0x007fd7d805bad8>) on POST request
         else 
             render json: {errors: garden.errors.full_messages}, status: unprocessible_entity
